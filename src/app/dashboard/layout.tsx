edbed0d312720de70400048e6b16891d7b566622
Lucas Thomaz <lucasmrthomaz@outlook.com>
2024-12-29 13:53:40 -0300

    Speed Insights module

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,9 +1,12 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
   return <section>
+    <SpeedInsights/>
     {children}
     </section>
 }
