f9d53819c3fa0bccaee43d6a69b2a87d26553cc4
Lucas Thomaz <lucasmrthomaz@outlook.com>
2025-03-06 17:46:20 -0300

    Refactor DashboardPage layout and update button link for folhas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,13 +6,12 @@ export default function DashboardPage() {
   return (
     <div className="p-5">
       <h1 className="text-3xl mb-5 font-sans font-thin">VISÃO RESUMIDA</h1>
-
-      <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 justify-evenly">
-
-        <Button variant="contained" color="primary" className="p-15" href="/dashboard/fatura">
+      
+      <Button variant="contained" color="primary" className="p-15 mt-5" href="/dashboard/folha">
           Todas as Folhas <ArrowForwardSharp />
         </Button>
 
+      <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 justify-evenly">
         <CardResume
           title="Folhas Abertas"
           descricao="Folhas que ainda não foram processadas ou enviadas ao destinatario."
@@ -27,10 +26,7 @@ export default function DashboardPage() {
           title="Folhas Fechadas"
           descricao="Folhas que foram enviadas com sucesso ao destinatario."
         />
-
       </div>
-
-
 
     </div>
   );
