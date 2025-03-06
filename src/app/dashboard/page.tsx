f2ad7d36fdc04f417ab321919e4c77a87050b339
Lucas Thomaz <lucasmrthomaz@outlook.com>
2025-03-06 18:09:54 -0300

    Fix API URL construction in funcionariosView and adjust layout in DashboardPage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,12 +6,12 @@ export default function DashboardPage() {
   return (
     <div className="p-5">
       <h1 className="text-3xl mb-5 font-sans font-thin">VISÃO RESUMIDA</h1>
-      
-      <Button variant="contained" color="primary" className="p-15 mt-10 mb-10" href="/dashboard/folha">
+
+      <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 justify-evenly">
+        <Button variant="contained" color="primary" className="p-15 mt-10 mb-10" href="/dashboard/folha">
           Todas as Folhas <ArrowForwardSharp />
         </Button>
 
-      <div className="flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5 justify-evenly">
         <CardResume
           title="Folhas Abertas"
           descricao="Folhas que ainda não foram processadas ou enviadas ao destinatario."
